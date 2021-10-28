66f19b127ee47328cc1d09acdc4d6dfe735152b4
MikrosDi <dimon96mik@mail.ru>
2021-10-28 19:53:58 +0300

    Добавьте файлы проекта.

---
+++ Blackjack_2/Blackjack_2.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include "Blackjack_2.h"
+
+int main()
+{
+    Card aceClubs(ACE, CLUBS);
+    Card aceHearts(ACE, HEARTS);
+    Card kingClubs(KING, CLUBS);
+
+    Hand playerHand;
+    playerHand.Add(&aceClubs);
+    playerHand.Add(&aceHearts);
+    playerHand.Add(&kingClubs);
+
+    int sum = playerHand.GetValue();
+    std::cout << "\n\nPlayer's hand value is: " << sum;
+    playerHand.Clear();
+    sum = playerHand.GetValue();
+    std::cout << "\nHand cleared! New value is: " << sum;
+}
+
+
